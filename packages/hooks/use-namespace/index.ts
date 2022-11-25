dd32c2f4d60e001a0263508dcc0835a0c8e2aa35
Potter <75838259@qq.com>
2022-11-25 15:15:00 +0800

    chore: project construction

---
+++ packages/hooks/use-namespace/index.ts
@@ -0,0 +1,116 @@
+export const defaultNamespace = 'el'
+const statePrefix = 'is-'
+
+const _bem = (
+  namespace: string,
+  block: string,
+  blockSuffix: string,
+  element: string,
+  modifier: string
+) => {
+  let cls = `${namespace}-${block}`
+  if (blockSuffix) {
+    cls += `-${blockSuffix}`
+  }
+  if (element) {
+    cls += `__${element}`
+  }
+  if (modifier) {
+    cls += `--${modifier}`
+  }
+  return cls
+}
+
+export const useNamespace = (block: string) => {
+  //TODO: 修改到此处?
+  const namespace = useGlobalConfig('namespace', defaultNamespace)
+
+  //namespace-block-blockSuffix
+  const b = (blockSuffix = '') =>
+    _bem(namespace.value, block, blockSuffix, '', '')
+
+  //namespace-block__element
+  const e = (element?: string) =>
+    element ? _bem(namespace.value, block, '', element, '') : ''
+
+  //namespace-block--modifier
+  const m = (modifier?: string) =>
+    modifier ? _bem(namespace.value, block, '', '', modifier) : ''
+
+  //namespace-block-blockSuffix__element
+  const be = (blockSuffix?: string, element?: string) =>
+    blockSuffix && element
+      ? _bem(namespace.value, block, blockSuffix, element, '')
+      : ''
+
+  //namespace-block__element--modifier
+  const em = (element?: string, modifier?: string) =>
+    element && modifier
+      ? _bem(namespace.value, block, '', element, modifier)
+      : ''
+
+  //namespace-block-blockSuffix--modifier
+  const bm = (blockSuffix?: string, modifier?: string) =>
+    blockSuffix && modifier
+      ? _bem(namespace.value, block, blockSuffix, '', modifier)
+      : ''
+
+  //namespace-block-blockSuffix__element--modifier
+  const bem = (blockSuffix?: string, element?: string, modifier?: string) =>
+    blockSuffix && element && modifier
+      ? _bem(namespace.value, block, blockSuffix, element, modifier)
+      : ''
+
+  //is-name
+  const is: {
+    (name: string, state: boolean | undefined): string
+    (name: string): string
+  } = (name: string, ...args: [boolean | undefined] | []) => {
+    const state = args.length >= 1 ? args[0]! : true
+    return name && state ? `${statePrefix}${name}` : ''
+  }
+
+  // for css var
+  // --el-xxx: value;
+  const cssVar = (object: Record<string, string>) => {
+    const styles: Record<string, string> = {}
+    for (const key in object) {
+      if (object[key]) {
+        styles[`--${namespace.value}-${key}`] = object[key]
+      }
+    }
+    return styles
+  }
+
+  // with block
+  // --el-block-key: value
+  const cssVarBlock = (object: Record<string, string>) => {
+    const styles: Record<string, string> = {}
+    for (const key in object) {
+      if (object[key]) {
+        styles[`--${namespace.value}-${block}-${key}`] = object[key]
+      }
+    }
+    return styles
+  }
+
+  const cssVarName = (name: string) => `--${namespace.value}-${name}`
+  const cssVarBlockName = (name: string) =>
+    `--${namespace.value}-${block}-${name}`
+
+  return {
+    namespace,
+    b,
+    e,
+    m,
+    be,
+    em,
+    bm,
+    bem,
+    is,
+    cssVar,
+    cssVarName,
+    cssVarBlock,
+    cssVarBlockName,
+  }
+}
