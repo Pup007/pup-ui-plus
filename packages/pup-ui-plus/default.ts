2dbfec03249a23726b0eb426edff1269b9b6757f
Potter <75838259@qq.com>
2022-12-09 15:03:43 +0800

    chore: mark progress location

--- packages/pup-ui-plus/default.ts
+++ packages/pup-ui-plus/default.ts
@@ -2,4 +2,6 @@ import { makeInstaller } from './make-installer'
 import Components from './component'
 import Plugins from './plugin'
 
+//TODO: 修改到此处?
+
 export default makeInstaller([...Components, ...Plugins])
