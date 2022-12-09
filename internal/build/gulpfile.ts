75ae3fb9385c28c89bb9d118bbc0590ed8c63c6d
Potter <75838259@qq.com>
2022-12-09 14:55:21 +0800

    build: copy css\type definition file to dist directory

--- internal/build/gulpfile.ts
+++ internal/build/gulpfile.ts
@@ -1,8 +1,40 @@
 import path from 'path'
 import { copyFile, mkdir } from 'fs/promises'
+import { copy } from 'fs-extra'
 import { parallel, series } from 'gulp'
-import { epOutput } from '@pup-ui-plus/build-utils'
-import { run, runTask, withTaskName } from './src'
+import {
+  buildOutput,
+  epOutput,
+  epPackage,
+  projRoot,
+} from '@pup-ui-plus/build-utils'
+import { buildConfig, run, runTask, withTaskName } from './src'
+
+import type { TaskFunction } from 'gulp'
+import type { Module } from './src'
+
+export const copyFiles = () =>
+  Promise.all([
+    copyFile(epPackage, path.join(epOutput, 'package.json')),
+    copyFile(
+      path.resolve(projRoot, 'README.md'),
+      path.resolve(epOutput, 'README.md')
+    ),
+    copyFile(
+      path.resolve(projRoot, 'global.d.ts'),
+      path.resolve(epOutput, 'global.d.ts')
+    ),
+  ])
+
+export const copyTypesDefinitions: TaskFunction = (done) => {
+  const src = path.resolve(buildOutput, 'types', 'packages')
+  const copyTypes = (module: Module) =>
+    withTaskName(`copyTypes:${module}`, () =>
+      copy(src, buildConfig[module].output.path, { recursive: true })
+    )
+
+  return parallel(copyTypes('esm'), copyTypes('cjs'))(done)
+}
 
 export const copyFullStyle = async () => {
   await mkdir(path.resolve(epOutput, 'dist'), { recursive: true })
@@ -20,17 +52,18 @@ export default series(
     //! 说明：打包packages所有模块代码
     runTask('buildModules'),
     runTask('buildFullBundle'),
-    runTask('generateTypesDefinitions')
-    /*    runTask('buildHelper'),
-     series(
-       withTaskName('buildThemeChalk', () =>
-         run('pnpm run -C packages/theme-chalk build')
-       ),
-       copyFullStyle
-     ) */
-  )
+    runTask('generateTypesDefinitions'),
+    //! TODO: 文档编译先关闭
+    // runTask('buildHelper'),
+    series(
+      withTaskName('buildThemeChalk', () =>
+        run('pnpm run -C packages/theme-chalk build')
+      ),
+      copyFullStyle
+    )
+  ),
 
-  // parallel(copyTypesDefinitions, copyFiles)
+  parallel(copyTypesDefinitions, copyFiles)
 )
 
 //! 说明：需要导出所有任务，否则gulp会包找不到对应执行任务
