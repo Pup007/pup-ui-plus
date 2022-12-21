4d3e836fbd05531b31f088e96955a271e9c04995
Potter <75838259@qq.com>
2022-12-21 14:15:35 +0800

    chore: perfect play

--- play/src/App.vue
+++ play/src/App.vue
@@ -1,19 +1,11 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { ElIcon } from '@pup-ui-plus/components'
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <el-icon>el-icon</el-icon>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
--- play/vite.config.ts
+++ play/vite.config.ts
@@ -1,7 +1,41 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import VueMacros from 'unplugin-vue-macros/vite'
+import path from 'path'
+import {
+  epPackage,
+  epRoot,
+  pkgRoot,
+  projRoot,
+} from '@pup-ui-plus/build-utils'
+
+import type { Plugin } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
+export default defineConfig(async ({ mode }) => {
+  return {
+    resolve: {
+      alias: [
+        {
+          find: /^pup-ui-plus(\/(es|lib))?$/,
+          replacement: path.resolve(epRoot, 'index.ts'),
+        },
+        {
+          find: /^pup-ui-plus\/(es|lib)\/(.*)$/,
+          replacement: `${pkgRoot}/$2`,
+        },
+      ],
+    },
+    plugins: [
+      VueMacros({
+        setupComponent: false,
+        setupSFC: false,
+        plugins: {
+          vue: vue(),
+          vueJsx: vueJsx(),
+        },
+      }),
+    ]
+  }
 })
