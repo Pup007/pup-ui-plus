390b7f5908acf63364df1595b69f60d0a4180d4c
Potter <75838259@qq.com>
2022-12-28 12:48:22 +0800

    docs: perfect docs

--- internal/build-constants/src/repo.ts
+++ internal/build-constants/src/repo.ts
@@ -1,4 +1,4 @@
-export const REPO_OWNER = 'pup-ui-plus'
+export const REPO_OWNER = 'Pup007'
 export const REPO_NAME = 'pup-ui-plus'
 export const REPO_PATH = `${REPO_OWNER}/${REPO_NAME}`
 export const REPO_BRANCH = 'master'
