a29b57d321905ae623cf51345a009f24a95fd4a3
Potter <75838259@qq.com>
2023-02-19 17:41:06 +0800

    chore: add comment

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -26,6 +26,7 @@ const alias: Alias[] = [
   },
 ]
 if (process.env.DOC_ENV !== 'production') {
+  //! 开发模式：进行路径转换，方便引用最新代码
   alias.push(
     {
       find: /^pup-ui-plus(\/(es|lib))?$/,
@@ -46,13 +47,14 @@ export default defineConfig(async ({ mode }) => {
   /* docs/package.json */
   const { dependencies: docsDeps } = getPackageDependencies(docPackage)
 
-  //TODO: 学习到此处?
+  //! 依赖优化
   const optimizeDeps = [...new Set([...epDeps, ...docsDeps])].filter(
     (dep) =>
       !dep.startsWith('@types/') &&
       !['@pup-ui-plus/metadata', 'pup-ui-plus'].includes(dep)
   )
 
+  //! 依赖优化：时间库
   optimizeDeps.push(
     ...(await glob(['dayjs/plugin/*.js'], {
       cwd: path.resolve(projRoot, 'node_modules'),
@@ -72,6 +74,7 @@ export default defineConfig(async ({ mode }) => {
       alias,
     },
     plugins: [
+      //! VueMacros：针对插件包装了一层，支持vite\rollup\esbuild等插件
       VueMacros({
         setupComponent: false,
         setupSFC: false,
@@ -80,15 +83,17 @@ export default defineConfig(async ({ mode }) => {
         },
       }),
 
-      // https://github.com/antfu/unplugin-vue-components
+      //! 自动导入组件插件：避免手动import组件和components定义组件
+      //相关文档：https://github.com/antfu/unplugin-vue-components
       Components({
+        //自动引入组件的位置
         dirs: ['.vitepress/vitepress/components'],
 
         allowOverrides: true,
 
         // custom resolvers
         resolvers: [
-          // auto import icons
+          // ! 自动导入说使用的图标
           // https://github.com/antfu/unplugin-icons
           IconsResolver(),
         ],
@@ -97,16 +102,22 @@ export default defineConfig(async ({ mode }) => {
         include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
       }),
 
+      //! 图标库：自动安装所使用的图标
       // https://github.com/antfu/unplugin-icons
       Icons({
         autoInstall: true,
       }),
+      //! 按需生成css引擎
+      // https://github.com/unocss/unocss
       UnoCSS(),
+      //! 转换markdown文件，自动注入headers + footers内容
       MarkdownTransform(),
+      //! 用于辅助插件开发
       Inspect(),
       mkcert(),
     ],
     optimizeDeps: {
+      //! 针对依赖优化进行预编译，加快打包构建过程
       include: optimizeDeps,
     },
   }
